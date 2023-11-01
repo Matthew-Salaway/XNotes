64b0ae7f26ba581dfb37be82e9145f650fe1d91e
Matthew-Salaway <matthewsalaway@gmail.com>
2023-11-01 11:47:02 -0700

    dataset download function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Step 1
-- Download the 5 files from https://twitter.com/i/communitynotes/download-data and store them in the data folder
+Run the download_datasets_into_data_folder() function in the data.py file
+- This downloads the 5 data files from https://twitter.com/i/communitynotes/download-data and stores them in the data folder
+- This is about 2.9 GB of data
+
--- data.py
+++ data.py
@@ -8,6 +8,9 @@
 # and returns a data frame with the necessary columns for training the model.
 
 import pandas as pd
+import os
+import requests
+
 
 notes_file_path = './Data/notes-00000.tsv'
 ratings_1_file_path = './Data/ratings-00000.tsv'
@@ -21,14 +24,52 @@ def import_and_combine_data_Rater_Model(necessary_columns):
 
     notes_df = pd.read_csv(notes_file_path, sep='\t', usecols=necessary_columns)
 
-notes_df = pd.read_csv(notes_file_path, sep='\t', usecols=['noteId', 'tweetId', 'summary'])
-note_status_df = pd.read_csv(note_status_history_file_path, sep='\t', usecols=['noteId', 'currentStatus'])
+def download_datasets_into_data_folder():
+    links = [
+    'https://ton.twimg.com/birdwatch-public-data/2023/11/01/notes/notes-00000.tsv',
+    'https://ton.twimg.com/birdwatch-public-data/2023/11/01/noteRatings/ratings-00001.tsv',
+    'https://ton.twimg.com/birdwatch-public-data/2023/11/01/noteRatings/ratings-00000.tsv',
+    'https://ton.twimg.com/birdwatch-public-data/2023/11/01/noteStatusHistory/noteStatusHistory-00000.tsv',
+    'https://ton.twimg.com/birdwatch-public-data/2023/11/01/userEnrollment/userEnrollment-00000.tsv'
+    ]
+    data_directory = 'Data'
+    # Create the directory if it doesn't exist
+    if not os.path.exists(data_directory):
+        os.makedirs(data_directory)
 
-result = pd.merge(notes_df[['noteId', 'tweetId', 'summary']], note_status_df[['noteId', 'currentStatus']], on='noteId')
+    # Nested function to download and save a file
+    def download_file(url, folder):
+        local_filename = url.split('/')[-1]
+        local_filepath = os.path.join(folder, local_filename)
 
-print(result.head())
-print(result.columns)
-print(result.shape)
-print(result.iloc[1])
+        # Stream the download (for large files)
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(local_filepath, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+
+        return local_filepath
+
+    # Download each file
+    for link in links:
+        try:
+            filepath = download_file(link, data_directory)
+            print(f"Downloaded {link} to {filepath}")
+        except requests.exceptions.HTTPError as err:
+            print(f"Failed to download {link}: {err}")
+
+
+
+
+# notes_df = pd.read_csv(notes_file_path, sep='\t', usecols=['noteId', 'tweetId', 'summary'])
+# note_status_df = pd.read_csv(note_status_history_file_path, sep='\t', usecols=['noteId', 'currentStatus'])
+
+# result = pd.merge(notes_df[['noteId', 'tweetId', 'summary']], note_status_df[['noteId', 'currentStatus']], on='noteId')
+
+# print(result.head())
+# print(result.columns)
+# print(result.shape)
+# print(result.iloc[1])
 
 # Use twitter API to get the tweet text from the tweetId column
