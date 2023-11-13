9277f279417a9e6fc4a6ae149a0b1e3f0c55d13d
Matthew-Salaway <matthewsalaway@gmail.com>
2023-11-13 13:38:26 -0800

    Human Rating Performance Metrics

--- data.py
+++ data.py
@@ -1,8 +1,4 @@
-# Download the files from the following link and save them in the Data folder
-# https://twitter.com/i/communitynotes/download-data
-# The files are named as follows: Notes data (1), Ratings data (1 & 2), Note status history data (1)
-
-# From these files, make a new data frame with the nessary columns for training the Rater model.
+# From these files, we will make a new data frame with the nessary columns for training the Rater model.
 
 # Make a function that uses pandas to import the files in the the Data folder
 # and returns a data frame with the necessary columns for training the model.
@@ -61,15 +57,19 @@ def download_datasets_into_data_folder():
 
 
 
+necessary_columns = {"notes": ['noteId', 'tweetId', 'summary'], 
+                     "noteStatusHistory": ['noteId', 'currentStatus'],
+                     }
 
-# notes_df = pd.read_csv(notes_file_path, sep='\t', usecols=['noteId', 'tweetId', 'summary'])
-# note_status_df = pd.read_csv(note_status_history_file_path, sep='\t', usecols=['noteId', 'currentStatus'])
 
-# result = pd.merge(notes_df[['noteId', 'tweetId', 'summary']], note_status_df[['noteId', 'currentStatus']], on='noteId')
+notes_df = pd.read_csv(notes_file_path, sep='\t', usecols=['noteId', 'tweetId', 'summary'])
+note_status_df = pd.read_csv(note_status_history_file_path, sep='\t', usecols=['noteId', 'currentStatus'])
 
-# print(result.head())
-# print(result.columns)
-# print(result.shape)
-# print(result.iloc[1])
+result = pd.merge(notes_df, note_status_df, on='noteId')
+
+print(result.head())
+print(result.columns)
+print(result.shape)
+print(result.iloc[1])
 
 # Use twitter API to get the tweet text from the tweetId column
---
+++ evaluation.py
@@ -0,0 +1,54 @@
+import pandas as pd
+ratings_1_file_path = './Data/ratings-00000.tsv'
+ratings_2_file_path = './Data/ratings-00000.tsv'
+note_status_history_file_path = './Data/noteStatusHistory-00000.tsv'
+
+def human_rater_performance():
+    rating_1_df = pd.read_csv(ratings_1_file_path, sep='\t', usecols=['noteId', 'helpfulnessLevel'])
+    rating_2_df = pd.read_csv(ratings_2_file_path, sep='\t', usecols=['noteId', 'helpfulnessLevel'])
+    note_status_df = pd.read_csv(note_status_history_file_path, sep='\t', usecols=['noteId', 'currentStatus'])
+
+    # Map note id to the current status. 0 if not helpful, 1 if helpful
+    note_id_to_status = {}
+    for i, row in note_status_df.iterrows():
+        if row['currentStatus'] == 'CURRENTLY_RATED_NOT_HELPFUL':
+            note_id_to_status[row['noteId']] = 0
+        elif row['currentStatus'] == 'CURRENTLY_RATED_HELPFUL':
+            note_id_to_status[row['noteId']] = 1
+    
+    print(note_id_to_status)
+
+
+    # iterate through all user ratings. if they have a note in the map see if its wrong or right
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    for i, row in rating_1_df.iterrows():
+        if row['noteId'] in note_id_to_status:
+            is_currently_helpful = note_id_to_status[row['noteId']]
+            if is_currently_helpful:
+                if row['helpfulnessLevel'] == 'HELPFUL':
+                    tp += 1
+                else:
+                    fn += 1
+            else:
+                if row['helpfulnessLevel'] == 'HELPFUL':
+                    fp += 1
+                else:
+                    tn += 1
+    print(tp, fp, tn, fn)
+
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    return {'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'Specificity': specificity, 'F1': f1}
+
+
+
+    
+
+print(human_rater_performance())
