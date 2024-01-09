ff161fb3540d7b2f8427beff3968ce1d85b2c084
Matthew-Salaway <matthewsalaway@gmail.com>
2024-01-09 09:18:39 -0800

    rater refactor

--- rater.py
+++ rater.py
@@ -38,15 +38,33 @@ def generate_prediction(tweets, notes, model_endpoint):
             results = results.append(0)
     return results
 
-def append_results_to_master(col_title, results, note_ids):
+def append_results_to_master(col_title, results_df, note_id_df):
     """
     Appends the results of the note rating to the master.csv in a new column called col_title
     """
-    # Iterate through the results, find the row with same note_id, put result in that row and col_title
+    master_csv_path = "../Data/master.csv"
+    
+    # Read the master CSV file into a DataFrame
+    master_df = pd.read_csv(master_csv_path)
+
+    # Merge the noteID DataFrame with the results DataFrame based on index
+    # Assuming the first (and only) column in results_df contains the results
+    combined_results_df = note_id_df.join(results_df)
+
+    # Merge the master DataFrame with the combined results DataFrame based on the noteID
+    # Assuming 'noteID' is the column in note_id_df and master_df for matching
+    merged_df = pd.merge(master_df, combined_results_df, how='left', left_on='noteID', right_index=True)
+
+    # Rename the results column
+    # Assuming the first (and only) column in results_df contains the results
+    result_column_name = results_df.columns[0]
+    merged_df.rename(columns={result_column_name: col_title}, inplace=True)
+
+    merged_df.to_csv("../Data.master.csv", index=False)
 
 
 
-def evaluate(current_statuses, predicted_statuses):
+def evaluate(current_statuses_df, predicted_statuses_df):
     """
     Prints evaluation metrics on the predicted rating and actual rating of the note. 
     """
@@ -74,21 +92,13 @@ def evaluate(current_statuses, predicted_statuses):
     return {'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'Specificity': specificity, 'F1': f1}
 
 
-# GPT Tubro Model Name: gpt-4-1106-preview
-
 if __name__=="__main__":
     # Get the tweets, notes, and model into generate. Take output and evaluate agaonst the actual. append these results to master
     df = pd.read_csv('master.csv')
     tweets = df['tweet_text']
     notes = df['original_note']
     model="gpt-4-1106-preview",
+    results_df = generate_prediction(tweets, notes, model)
+    evaluate(df["currentStatus"], results_df)
+    append_results_to_master("zero_shot_gpt_4_turbo", results_df, df["noteID"])
 
-    # ratings_df = generate_prediction(df)
-    # to_csv(ratings_df)
-    # accuracy = evaluate(ratings_df)
-    # print('-'*50 + 'Accuracy' + '-'*50)
-    # print(accuracy)
-    # eval = evaluate_more(ratings_df)
-    # print('-'*50 + 'Evaluatiion' + '-'*50)
-    # print(eval)
-    # Accuracy: 71.91%
